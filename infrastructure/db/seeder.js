e5f373adf1accec124583a1cf161ce20dde74df9
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2020-09-27 16:40:52 -0400

    create seeder and first collection

---
+++ infrastructure/db/seeder.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const { Seeder } = require("mongo-seeding");
+const config = require("../config");
+
+const seederConfig = {
+  database: config.MONGO_URI,
+  dropDatabase: true,
+};
+const seeder = new Seeder(seederConfig);
+const collections = seeder.readCollectionsFromPath(
+  path.resolve("./infrastructure/db/data-import")
+);
+
+async function seed() {
+  try {
+    console.log("Starting database seeding process...");
+    await seeder.import(collections);
+    console.log("Seeding success!");
+  } catch (err) {
+    console.error("Seeding Error:", err);
+  }
+}
+
+seed();

945412a7c4912bd555259380bf7a81adae98a433
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2020-09-28 05:18:53 -0400

    refactor server errors handler

---
+++ infrastructure/http/errorsHandler.js
@@ -0,0 +1,30 @@
+const createMessage = (code) => {
+  const errorCodeDescriptions = {
+    400: "Bad Request. The request lacks the correct parameters",
+    404: "The requested endpoint was not found",
+    405: "HTTP Method Not Allowed",
+  };
+
+  const error = {
+    error: true,
+    errorCode: code,
+    message: errorCodeDescriptions[code],
+  };
+
+  return JSON.stringify(error);
+};
+
+const handleError = (code, response) => {
+  response.writeHead(code, {
+    "Content-Type": "application/json;charset=utf-8",
+  });
+
+  const message = createMessage(code);
+  console.error(message);
+  response.end(message);
+  return message;
+};
+
+module.exports = errorsHandler = {
+  handleError,
+};
